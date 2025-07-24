ce202c99d1b9813e650ff7777561a88dcaf9415b
Azlan-saja <azlansaja19@gmail.com>
2025-07-24 15:44:22 +0700

    p5

--- lib/view/login_view.dart
+++ lib/view/login_view.dart
@@ -6,39 +6,80 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              // 1. Gambar,
-              // 2. Teks Judul,
-              Text(
-                "Notes App",
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.teal,
-                  fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                // 1. Gambar,
+                Image.asset(
+                  "lib/assets/images/login.png",
+                  width: 300,
                 ),
-              ),
-              // 3. Inputan Username,
-              TextFormField(
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.people),
-                  hintText: "Username",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    borderSide: BorderSide.none,
+                // 2. Teks Judul,
+                Text(
+                  "Notes App",
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Theme.of(context).colorScheme.primary,
+                    fontWeight: FontWeight.bold,
                   ),
-                  filled: true,
-                  fillColor: Colors.teal.withAlpha(50),
-                  contentPadding: EdgeInsets.only(top: 13),
                 ),
-              ),
-              // 4. Inputan Password,
-              // 5. Tombol Login,
-              // 6. Teks Buat Akun,
-            ],
+                // 3. Inputan Username,
+                TextFormField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.people),
+                    hintText: "Username",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide.none,
+                    ),
+                    filled: true,
+                    fillColor: Theme.of(context).colorScheme.surfaceContainer,
+                    contentPadding: EdgeInsets.only(top: 13),
+                  ),
+                ),
+                // 4. Inputan Password,
+                SizedBox(height: 14),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.key),
+                    suffixIcon: Icon(Icons.visibility),
+                    hintText: "Password",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide.none,
+                    ),
+                    filled: true,
+                    fillColor: Theme.of(context).colorScheme.surfaceContainer,
+                    contentPadding: EdgeInsets.only(top: 13),
+                  ),
+                ),
+                // 5. Tombol Login,
+                SizedBox(height: 16),
+                SizedBox(
+                  width: double.infinity,
+                  child: FilledButton.icon(
+                    onPressed: () {},
+                    label: Text("Login"),
+                    icon: Icon(Icons.login),
+                  ),
+                ),
+                // 6. Teks Buat Akun,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Don't have an account?"),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text("Create account"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
